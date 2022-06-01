176bb090d8abd1feeef3d79f989025338cdd48da
Isaac Anane Amankwaah <57358941+brasmith9@users.noreply.github.com>
2022-06-01 14:50:47 +0000

    it always looks doable until you begin🤪

--- card-component/index.js
+++ card-component/index.js
@@ -14,14 +14,13 @@ const styles = {
         element.style.boxSizing = "border-box";
         element.style.position = "absolute";
         element.style.width = "400px";
-        element.style.height = "340px";
         element.style.left = "70px";
         element.style.top = "280px";
         element.style.background = "#FFF";
         element.style.border = "1px solid rgba(0, 0, 0, 0.1)";
         element.style.boxShadow = "0px 5px 10px 1px rgba(0, 0, 0, 0.15)";
         element.style.borderRadius = "20px";
-        element.style.padding = "20px";
+        element.style.padding = "20px 30px";
       }
   },
   title: {
@@ -36,8 +35,41 @@ const styles = {
   emailWrapper: {
     set setStyle(element) {
       element.style.display = "flex";
-      element.style.displayDirection = "column";
+      element.style.flex = "0 0 auto";
+      element.style.flexFlow = "row wrap";
+      element.style.alignItems = "center";
+      element.style.marginBottom = "0";
 
+    }
+  },
+  label: {
+    set setStyle(element) {
+      element.style.display = "block";
+      element.style.fontSize = "12px";
+      element.style.lineHeight = "12px";
+      element.style.color = "rgba(0, 0, 0, 0.8)";
+      element.style.marginBottom = "5px";
+
+    }
+  },
+  formControl: {
+    set setStyle(element){
+      element.style.background = "#fff";
+      element.style.border = "1px solid rgba(0, 0, 0, 0.1)";
+      element.style.width = "100%";
+      element.style.height = "40px";
+      element.style.marginBottom = "12px";
+
+    }
+  },
+  button:{
+    set setStyle(element) {
+      element.style.width = "100%";
+      element.style.height = "55px";
+      element.style.background = "#2D2D2D";
+      element.style.color = "#fff";
+      element.style.borderRadius = "6px";
+      element.style.border = "none";
     }
   }
 
@@ -55,6 +87,10 @@ let welcomeForm = document.createElement("form");
 let welcomeEmailWrapper = document.createElement("div");
 let welcomeEmailLabel = document.createElement("label");
 let welcomeEmailinput = document.createElement("input");
+let welcomeUsernameLabel = document.createElement("label");
+let welcomeUsernameInput = document.createElement("input");
+let welcomeButton = document.createElement("button");
+
 
 //Apply Attributes here
 cardWrapper.setAttribute("class", "card-wrapper");
@@ -65,16 +101,24 @@ subtitle.setAttribute("class", "subtitle");
 welcomeCardBody.setAttribute("class", "card-body");
 welcomeForm.setAttribute("method", "post");
 welcomeForm.setAttribute("action", "#");
-welcomeEmailWrapper.setAttribute("class", "form-control");
+welcomeEmailWrapper.setAttribute("class", "form-group");
 welcomeEmailLabel.setAttribute("class", "form-label");
-
+welcomeEmailinput.setAttribute("class", "form-control");
+welcomeEmailinput.setAttribute("type", "email");
+welcomeButton.setAttribute("type", "submit");
 
 
 //Apply Styles here
-cardWrapper.style.border = "1px solid #000";
 styles.card.setStyle = welcomeCard;
 styles.title.setStyle = welcomeTitle;
-styles.emailWrapper.setStyle = welcomeEmailWrapper;
+
+
+// styles.emailWrapper.setStyle = welcomeEmailWrapper;
+styles.label.setStyle = welcomeEmailLabel
+styles.label.setStyle = welcomeUsernameLabel
+styles.formControl.setStyle = welcomeEmailinput
+styles.formControl.setStyle = welcomeUsernameInput
+styles.button.setStyle = welcomeButton
 
 
 //Apend them to the DOM here
@@ -87,9 +131,15 @@ welcomeCard.appendChild(welcomeCardBody);
 welcomeCardBody.appendChild(welcomeForm);
 welcomeForm.appendChild(welcomeEmailWrapper);
 welcomeEmailWrapper.appendChild(welcomeEmailLabel);
+welcomeEmailWrapper.appendChild(welcomeUsernameLabel);
 welcomeEmailWrapper.appendChild(welcomeEmailinput);
+welcomeEmailWrapper.appendChild(welcomeUsernameLabel);
+welcomeEmailWrapper.appendChild(welcomeUsernameInput);
+welcomeEmailWrapper.appendChild(welcomeButton);
 
 //Apply Text to elements
 welcomeTitle.innerHTML = "Welcome User";
 subtitle.innerHTML = "Let's begin setting up your account 🚀";
 welcomeEmailLabel.innerHTML = "Email";
+welcomeUsernameLabel.innerHTML = "Username";
+welcomeButton.innerHTML = "Next";
